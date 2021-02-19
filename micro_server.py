e988ec3206d28c257150d21495d4cfddecdd57b2
Joe Polaski <polaski.joseph@gmail.com>
2021-02-19 16:32:52 -0500

    Added proper citations

--- micro_server.py
+++ micro_server.py
@@ -1,3 +1,18 @@
+"""
+Sources Cited:
+Title: Python Socket Programming Tutorial
+Author: Tech With Tim
+URL: https://www.youtube.com/watch?v=3QiPPX-KeSc&t=2593s
+Description: We followed this tutorial and used/refactored the code into our
+own implementation to build the server as a prtable class.
+
+Sources Cited:
+Title: Sockets Tutorial with Python 3 part 3 - sending and receiving Python Objects w/ Pickle
+Author: HSKinsley (Sentdex)
+URL: https://www.youtube.com/c/sentdex/about
+Description: We followed this tutorial and used it as a reference for using
+pickling with python sockets
+"""
 import socket
 import pickle
 
@@ -30,8 +45,7 @@ class micro_server:
         connected = True
         while connected:
             # fetch length of incoming message from header
-            data_length = self.get_data_length()
-            
+            data_length = self.get_data_length()            
     
     def get_data_length(self):
         """helper to handle_client_requests()"""
