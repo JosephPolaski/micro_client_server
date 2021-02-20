7eb8a2335637143ee8a6c2f1a5f7542b01ce822a
morrimeg <morrimeg@oregonstate.edu>
2021-02-19 23:04:11 -0600

    added a way to talk to the server, added a method to send header, added a method to send message

--- micro_client.py
+++ micro_client.py
@@ -16,12 +16,47 @@ pickling with python sockets
 import socket
 import pickle
 
+
 class micro_server:
-    
-    self.__init__():
-        self.__HEAD = 64 # header of 64 bytes for message protocol
+    def __init__(self):
+        #self.__init__():
+        self.__HEAD = 64  # header of 64 bytes for message protocol
         self.__PORT = 5467
         self.__IP = socket.gethostbyname(socket.gethostname())
-        self.__socket = self.define_micro_socket()
-        self.start_listening()
+        self.__ADDR = (self.__IP, self.__PORT)
+        self.__socket = self.define_micro_socket_client()
+    #self.start_listening()
+    
+    def define_micro_socket_client(self):
+        """Create and bind socket to port 5467"""
+        micro_socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        micro_socket_client.connect(self.__ADDR)
+        return micro_socket_client
+
+    def send_header(self, header_msg):
+        """Sends the header message to the server"""
+            message = header_msg.encode('utf-8')
+            message_length = len(message)
+            send_length = str(message_length).encode('utf-8')
+            send_length += b' ' * (self.__HEAD - len(send_length))
+            self.__socket.send(send_length)
+            self.__socket.send(message)
+    
+    def send_message(self, msg):
+        """Sends the message to the server"""
+        message = msg.encode('utf-8')
+        message_length = len(message)
+        send_length = str(message_length).encode('utf-8')
+        send_length += b' ' * (self.__HEAD - len(send_length))
+        self.__socket.send(send_length)
+        self.__socket.send(message)
+
+
+# using this for testing
+def main():
+    client = micro_server()
+    client.send_header("life generator")
+
+main()
+
 
