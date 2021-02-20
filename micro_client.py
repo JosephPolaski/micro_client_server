832462900f2c4fcde293fa2a9ba0fa92664ca033
Joe Polaski <polaski.joseph@gmail.com>
2021-02-20 14:25:23 -0500

    Corrected a few serverside data processing issues. Refactored client to remove redundant method and renamed class to micro_client. All communication currently functioning properly.

--- micro_client.py
+++ micro_client.py
@@ -17,46 +17,39 @@ import socket
 import pickle
 
 
-class micro_server:
+class micro_client:
     def __init__(self):
         #self.__init__():
         self.__HEAD = 64  # header of 64 bytes for message protocol
         self.__PORT = 5467
         self.__IP = socket.gethostbyname(socket.gethostname())
         self.__ADDR = (self.__IP, self.__PORT)
+        self.__DCON = "&END"
         self.__socket = self.define_micro_socket_client()
-    #self.start_listening()
     
     def define_micro_socket_client(self):
         """Create and bind socket to port 5467"""
         micro_socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         micro_socket_client.connect(self.__ADDR)
         return micro_socket_client
-
-    def send_header(self, header_msg):
-        """Sends the header message to the server"""
-            message = header_msg.encode('utf-8')
-            message_length = len(message)
-            send_length = str(message_length).encode('utf-8')
-            send_length += b' ' * (self.__HEAD - len(send_length))
-            self.__socket.send(send_length)
-            self.__socket.send(message)
     
     def send_message(self, msg):
         """Sends the message to the server"""
-        message = msg.encode('utf-8')
+        message = msg.encode('utf-8') 
         message_length = len(message)
+        
         send_length = str(message_length).encode('utf-8')
         send_length += b' ' * (self.__HEAD - len(send_length))
-        self.__socket.send(send_length)
-        self.__socket.send(message)
+      
+        self.__socket.send(send_length) # send header with message length
+        self.__socket.send(message) # send request message content
 
 
-# using this for testing
-def main():
-    client = micro_server()
-    client.send_header("life generator")
+# main function for test
+if __name__ == "__main__":
+    client = micro_client()
+    client.send_message("life generator")
+    client.send_message('&END')
 
-main()
 
 
--- micro_server.py
+++ micro_server.py
@@ -18,14 +18,24 @@ import pickle
 
 
 class micro_server:
-    def __init__(self):
-        #self.__init__():
+    
+    def __init__(self, call_back):
+        self.define_data_members(call_back)
+        self.start_listening()
+
+    def define_data_members(self, call_back):
+        self.define_address_members()
+
+        # define end protocol, call back function and socket
+        self.__DCON = "&END"
+        self.__call_back = call_back
+        self.__socket = self.define_micro_socket()
+    
+    def define_address_members(self):
         self.__HEAD = 64  # header of 64 bytes for message protocol
         self.__PORT = 5467
         self.__IP = socket.gethostbyname(socket.gethostname())
         self.__ADDR = (self.__IP, self.__PORT)
-        self.__socket = self.define_micro_socket()
-        self.start_listening()
     
     def define_micro_socket(self):
         """Create and bind socket to port 5467"""
@@ -39,7 +49,10 @@ class micro_server:
         while True:
             # blocks until connection to socket is detected
             client, address = self.__socket.accept()
-            self.handle_client_requests(client, address)
+            request = self.handle_client_requests(client, address)
+
+            # check for closing request
+            client.close() if request == self.__DCON else self.__call_back(request)
 
     def handle_client_requests(self, client, address):
         print(f"New Connection from {address}")
@@ -48,34 +61,36 @@ class micro_server:
         while connected:
             # fetch length of incoming message from header
             data_length = self.get_data_length(client)
+           
+            # fetch incoming request
+            request = self.get_actual_data(client, data_length)
+            return request            
 
     def get_data_length(self, client):
         """helper to handle_client_requests()"""
         # blocks until request header recieved
-        data = client.recv(self.__HEAD).decode('utf-8')
-        print(data) # TAKE OUT
+        data_length = client.recv(self.__HEAD).decode('utf-8')
         
         # cast decoded header as integer length
-        data_length = int(len(data))
-        print(data_length) # TAKE OUT
+        data_length_int = int(data_length)     
         
-        return data_length, data
-    
-    def get_actual_data(self, client, data):
+        return data_length_int
+
+    def get_actual_data(self, client, data_length):
         """helper to handle_client_requests()"""
         # blocks until request data recieved
-        raw_data = client.recv(data).decode('utf-8')
-        
-        return raw_data
+        request = client.recv(data_length).decode('utf-8')        
+        return request
 
+# main function for test
+if __name__ == "__main__":
 
-def main():
-    server = micro_server()
-# data_len, text = server.get_data_length()
-# print(data_len)
-# print(text)
+    # test callback function
+    def call_back(request):
+        print(f'request is {request}')
 
-main()
+    server = micro_server(call_back)
+
 
     
 
