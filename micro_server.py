352306ed2cc65b5aa4505932ba1f81a243caaf8e
Joe Polaski <46409181+JosephPolaski@users.noreply.github.com>
2021-03-14 20:13:05 -0400

    Updated micro_server.py

--- micro_server.py
+++ micro_server.py
@@ -12,13 +12,6 @@ Author: Tech With Tim
 URL: https://www.youtube.com/watch?v=3QiPPX-KeSc&t=2593s
 Description: We followed this tutorial and used/refactored the code into our
 own implementation to build the server as a portable class.
-
-Sources Cited:
-Title: Sockets Tutorial with Python 3 part 3 - sending and receiving Python Objects w/ Pickle
-Author: HSKinsley (Sentdex)
-URL: https://www.youtube.com/c/sentdex/about
-Description: We followed this tutorial and used it as a reference for using
-pickling with python sockets
 """
 import socket
 
